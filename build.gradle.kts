87a59319aedfec9e0e4e767706455947ae7be5b8
Kristian Frøhlich <186453+kfh@users.noreply.github.com>
2025-03-25 10:24:05 +0100

    Added Gradle configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.10"
-    kotlin("plugin.serialization") apply true
+    kotlin("plugin.serialization") version "2.1.10"
     id("maven-publish")
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
     id("org.jlleitschuh.gradle.ktlint-idea") version "11.6.1"
@@ -19,15 +19,6 @@ publishing {
             artifactId = "emottak-utils"
             version = "0.0.9"
             from(components["java"])
-        }
-    }
-    repositories {
-        maven {
-            url = uri("https://maven.pkg.github.com/navikt/${rootProject.name}")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
         }
     }
 }
