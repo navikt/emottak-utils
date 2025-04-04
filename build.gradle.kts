d2f55b17d9785b753b5344ba028518a42d637caf
Terje Nilssen <terje.nilssen@nav.no>
2025-04-04 14:34:19 +0200

    Kopiert VaultUtil.kt fra ebxml-processor sin felles-modul (#4)
    * Kopiert VaultUtil.kt fra ebxml-processor => felles => no.nav.emottak.vault.VaultUtil.kt
    
    Har også lagt inn følgende operasjoner:
    - getVaultServiceUser(): Skal benyttes av ebms-asynch-router (og flere) til å hente servicebruker når vi migrerer til distroless.
    - readVaultPathData(): Brukes til å slå opp en Vault-path, hvor man har bruk for å hente flere ressurser (for å slippe repiterende oppslag for hver ressurs).
    - getClient(): Behøves av smtp-transport.
    
    * Oppdatert versjonsnummer.

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ publishing {
         create<MavenPublication>("mavenJava") {
             groupId = "no.nav.emottak"
             artifactId = "emottak-utils"
-            version = "0.2.0"
+            version = "0.2.1"
             from(components["java"])
         }
     }
@@ -39,10 +39,12 @@ publishing {
 }
 
 dependencies {
+    implementation(libs.slf4j)
     implementation(libs.kotlin.kafka)
     implementation(libs.hoplite.core)
     implementation(libs.arrow.fx.coroutines)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.vault.java.driver)
     testImplementation(kotlin("test"))
     testImplementation(testLibs.bundles.kotest)
     testImplementation(testLibs.kotest.extensions.testcontainers)
