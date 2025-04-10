1409ac7546f978b39709f2fba901fcb479fe4d2a
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2025-04-10 15:23:39 +0200

    Tidssone fix

--- src/main/kotlin/no/nav/emottak/utils/kafka/model/Event.kt
+++ src/main/kotlin/no/nav/emottak/utils/kafka/model/Event.kt
@@ -5,6 +5,8 @@ import kotlinx.serialization.json.Json
 import no.nav.emottak.utils.serialization.InstantSerializer
 import no.nav.emottak.utils.serialization.UuidSerializer
 import java.time.Instant
+import java.time.ZoneId
+import java.time.ZonedDateTime
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
@@ -18,7 +20,7 @@ data class Event(
     val messageId: String,
     val eventData: String? = null,
     @Serializable(with = InstantSerializer::class)
-    val createdAt: Instant = Instant.now()
+    val createdAt: Instant = ZonedDateTime.now(ZoneId.of("Europe/Oslo")).toInstant()
 ) {
     fun toByteArray(): ByteArray {
         return Json.encodeToString(this).toByteArray()
