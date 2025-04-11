120a91d5965f7a8f7820cf87544fd22240610d43
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2025-04-11 11:16:30 +0200

    Lagt til tester

---
+++ src/test/kotlin/no/nav/emottak/utils/ExtensionFunctionsTest.kt
@@ -0,0 +1,23 @@
+package no.nav.emottak.utils
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+
+class ExtensionFunctionsTest : StringSpec({
+    "parseOrGenerateUuid() should parse a valid UUID string" {
+        val validUuidString = "123e4567-e89b-12d3-a456-426614174000"
+
+        val result = validUuidString.parseOrGenerateUuid()
+
+        result.toString() shouldBe validUuidString
+    }
+
+    "parseOrGenerateUuid() should generate a new UUID for an invalid UUID string" {
+        val invalidUuidString = "invalid-uuid"
+
+        val result = invalidUuidString.parseOrGenerateUuid()
+
+        result.toString() shouldNotBe invalidUuidString
+    }
+})
