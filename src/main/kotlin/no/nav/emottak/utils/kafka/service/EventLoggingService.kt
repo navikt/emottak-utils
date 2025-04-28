ed3122b08ed9fc468a3ef979a67b08f9b9a63778
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2025-04-28 16:46:34 +0200

    Utført utsending av meldingsdetaljer til en separat topic

--- src/main/kotlin/no/nav/emottak/utils/kafka/service/EventLoggingService.kt
+++ src/main/kotlin/no/nav/emottak/utils/kafka/service/EventLoggingService.kt
@@ -1,17 +1,19 @@
 package no.nav.emottak.utils.kafka.service
 
+import no.nav.emottak.utils.config.EventLogging
 import no.nav.emottak.utils.kafka.client.EventPublisherClient
 import no.nav.emottak.utils.kafka.model.Event
-import no.nav.emottak.utils.kafka.model.EventMessageDetails
+import no.nav.emottak.utils.kafka.model.MessageDetails
 import org.apache.kafka.clients.producer.RecordMetadata
 
 class EventLoggingService(
+    private val eventLoggingConfig: EventLogging,
     private val kafkaPublisherClient: EventPublisherClient
 ) {
 
     suspend fun logEvent(event: Event): Result<RecordMetadata> =
-        kafkaPublisherClient.publishMessage(event.toByteArray())
+        kafkaPublisherClient.publishMessage(eventLoggingConfig.eventTopic, event.toByteArray())
 
-    suspend fun logEventMessageDetails(eventMessageDetails: EventMessageDetails): Result<RecordMetadata> =
-        kafkaPublisherClient.publishMessage(eventMessageDetails.toByteArray())
+    suspend fun logMessageDetails(messageDetails: MessageDetails): Result<RecordMetadata> =
+        kafkaPublisherClient.publishMessage(eventLoggingConfig.messageDetailsTopic, messageDetails.toByteArray())
 }
