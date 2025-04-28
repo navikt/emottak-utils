f032836e1105b2eab556f319cf2656062fad06ea
terjenilssen <terje.nilssen@nav.no>
2025-04-28 13:10:34 +0200

    Grensesnittet 'vault-java-driver' fra 'com.bettercloud' bør være tilgjengelig for konsumenter av emottak-utils, slik at de kan bruke responsobjektet LogicalResponse fra VaultUtil.getClient().logical().read(path)

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     implementation(libs.hoplite.core)
     implementation(libs.arrow.fx.coroutines)
     implementation(libs.kotlinx.serialization.json)
-    implementation(libs.vault.java.driver)
+    api(libs.vault.java.driver)
     testImplementation(kotlin("test"))
     testImplementation(testLibs.bundles.kotest)
     testImplementation(testLibs.kotest.extensions.testcontainers)
