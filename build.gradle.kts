954fe9cf866969495221ea2f67a0f0f5d6164a2a
terjenilssen <terje.nilssen@nav.no>
2025-07-30 15:09:26 +0200

    Endret verdien til LogFields.TO_ROLE fra "role" til "toRole"

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ publishing {
         create<MavenPublication>("mavenJava") {
             groupId = "no.nav.emottak"
             artifactId = "emottak-utils"
-            version = "0.3.1"
+            version = "0.3.2"
             from(components["java"])
         }
     }
