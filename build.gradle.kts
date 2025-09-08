ba182cefa8d3a4eb284b08385d235452a3ae3a56
terjenilssen <terje.nilssen@nav.no>
2025-09-08 13:02:19 +0200

    Klargjort for ny release

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ publishing {
         create<MavenPublication>("mavenJava") {
             groupId = "no.nav.emottak"
             artifactId = "emottak-utils"
-            version = "0.3.4"
+            version = "0.3.5"
             from(components["java"])
         }
     }
