c5872ea2a64b41ab3620ec61766f501dd978ff85
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2025-10-08 16:46:18 +0200

    Satt opp dev utgave for biiblioteket

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ publishing {
         create<MavenPublication>("mavenJava") {
             groupId = "no.nav.emottak"
             artifactId = "emottak-utils"
-            version = "0.3.5"
+            version = "0.3.5.dev1"
             from(components["java"])
         }
     }
