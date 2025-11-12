bf584c27d31c457c7a4431d12d484ae2594685c9
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2025-11-12 10:21:31 +0100

    Lagt til debug logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ publishing {
         create<MavenPublication>("mavenJava") {
             groupId = "no.nav.emottak"
             artifactId = "emottak-utils"
-            version = "0.3.5.dev10"
+            version = "0.3.5.dev11"
             from(components["java"])
         }
     }
--- src/main/kotlin/no/nav/emottak/utils/edi2/EdiAdapterClient.kt
+++ src/main/kotlin/no/nav/emottak/utils/edi2/EdiAdapterClient.kt
@@ -18,66 +18,95 @@ import no.nav.emottak.utils.edi2.models.Message
 import no.nav.emottak.utils.edi2.models.PostAppRecRequest
 import no.nav.emottak.utils.edi2.models.PostMessageRequest
 import no.nav.emottak.utils.edi2.models.StatusInfo
+import org.slf4j.Logger
 import kotlin.uuid.Uuid
 
-class EdiAdapterClient(private val ediAdapterUrl: String, clientProvider: () -> HttpClient) {
+class EdiAdapterClient(
+    private val ediAdapterUrl: String,
+    private val clientProvider: () -> HttpClient,
+    private val log: Logger
+) {
     private var httpClient = clientProvider.invoke()
 
     suspend fun getApprecInfo(id: Uuid): Pair<List<ApprecInfo>?, ErrorMessage?> {
-        val response = httpClient.get("$ediAdapterUrl/api/v1/messages/$id/apprec") {
+        val url = "$ediAdapterUrl/api/v1/messages/$id/apprec"
+        val response = httpClient.get(url) {
             contentType(ContentType.Application.Json)
         }
+        log.debug("EDI2 test: Response from $url : $response")
+
         return handleResponse(response)
     }
 
     suspend fun getMessages(getMessagesRequest: GetMessagesRequest): Pair<List<Message>?, ErrorMessage?> {
-        val response = httpClient.get("$ediAdapterUrl/api/v1/messages?${getMessagesRequest.toUrlParams()}") {
+        val url = "$ediAdapterUrl/api/v1/messages?${getMessagesRequest.toUrlParams()}"
+        val response = httpClient.get(url) {
             contentType(ContentType.Application.Json)
         }
+        log.debug("EDI2 test: Response from $url : $response")
+
         return handleResponse(response)
     }
 
     suspend fun postMessage(postMessagesRequest: PostMessageRequest): Pair<String?, ErrorMessage?> {
-        val response = httpClient.post("$ediAdapterUrl/api/v1/messages") {
+        val url = "$ediAdapterUrl/api/v1/messages"
+        val response = httpClient.post(url) {
             contentType(ContentType.Application.Json)
             setBody(postMessagesRequest)
         }
+        log.debug("EDI2 test: Response from $url : $response")
+
         return handleResponse(response)
     }
 
     suspend fun getMessage(id: Uuid): Pair<Message?, ErrorMessage?> {
-        val response = httpClient.get("$ediAdapterUrl/api/v1/messages/$id") {
+        val url = "$ediAdapterUrl/api/v1/messages/$id"
+        val response = httpClient.get(url) {
             contentType(ContentType.Application.Json)
         }
+        log.debug("EDI2 test: Response from $url : $response")
+
         return handleResponse(response)
     }
 
     suspend fun getBusinessDocument(id: Uuid): Pair<GetBusinessDocumentResponse?, ErrorMessage?> {
-        val response = httpClient.get("$ediAdapterUrl/api/v1/messages/$id/document") {
+        val url = "$ediAdapterUrl/api/v1/messages/$id/document"
+        val response = httpClient.get(url) {
             contentType(ContentType.Application.Json)
         }
+        log.debug("EDI2 test: Response from $url : $response")
+
         return handleResponse(response)
     }
 
     suspend fun getMessageStatus(id: Uuid): Pair<List<StatusInfo>?, ErrorMessage?> {
-        val response = httpClient.get("$ediAdapterUrl/api/v1/messages/$id/status") {
+        val url = "$ediAdapterUrl/api/v1/messages/$id/status"
+        val response = httpClient.get(url) {
             contentType(ContentType.Application.Json)
         }
+        log.debug("EDI2 test: Response from $url : $response")
+
         return handleResponse(response)
     }
 
     suspend fun postApprec(id: Uuid, apprecSenderHerId: Int, postAppRecRequest: PostAppRecRequest): Pair<String?, ErrorMessage?> {
-        val response = httpClient.post("$ediAdapterUrl/api/v1/messages/$id/apprec/$apprecSenderHerId") {
+        val url = "$ediAdapterUrl/api/v1/messages/$id/apprec/$apprecSenderHerId"
+        val response = httpClient.post(url) {
             contentType(ContentType.Application.Json)
             setBody(postAppRecRequest)
         }
+        log.debug("EDI2 test: Response from $url : $response")
+
         return handleResponse(response)
     }
 
     suspend fun markMessageAsRead(id: Uuid, herId: Int): Pair<Boolean?, ErrorMessage?> {
-        val response = httpClient.put("$ediAdapterUrl/api/v1/messages/$id/read/$herId") {
+        val url = "$ediAdapterUrl/api/v1/messages/$id/read/$herId"
+        val response = httpClient.put(url) {
             contentType(ContentType.Application.Json)
         }
+        log.debug("EDI2 test: Response from $url : $response")
+
         return if (response.status == HttpStatusCode.NoContent) {
             Pair(true, null)
         } else {
