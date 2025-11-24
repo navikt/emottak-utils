1bcc8fc371d43f3c9efe6ee5e4c69181a1c271e4
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2025-11-24 09:30:20 +0100

    Fikset skrivefeil

--- src/main/kotlin/no/nav/emottak/utils/edi2/models/DeliveryState.kt
+++ src/main/kotlin/no/nav/emottak/utils/edi2/models/DeliveryState.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Serializable
 enum class DeliveryState(val value: String, val description: String) {
     UNCONFIRMED("Unconfirmed", "Transport is not confirmed"),
     ACKNOWLEDGED("Acknowledged", "Transport is confirmed. Equivalent to 'Acknowledgement' without severe 'eb:ErrorList/eb:HighestSeverity'"),
-    REJECTED("Rejected", "Transport is rejected. Equivalient to 'MessageError' with 'eb:ErrorList/eb:HighestSeverity'=\"Error\""),
+    REJECTED("Rejected", "Transport is rejected. Equivalent to 'MessageError' with 'eb:ErrorList/eb:HighestSeverity'=\"Error\""),
     UNKNOWN("Unknown", "The value is not supported");
 
     companion object {
