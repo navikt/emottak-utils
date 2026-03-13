cc752f061547c76dd38871da9dbb6341c26b04e5
Aage Kramarics <Aage.Kramarics@nav.no>
2026-03-13 12:12:24 +0100

    Bump versjonsnr

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ publishing {
         create<MavenPublication>("mavenJava") {
             groupId = "no.nav.emottak"
             artifactId = "emottak-utils"
-            version = "0.3.6"
+            version = "0.3.7"
             from(components["java"])
         }
     }
