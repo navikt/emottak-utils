ead85fe5950eaafe81d1d3a31f6d69aa6e9ca433
Thomas Burnett <thburnett@users.noreply.github.com>
2026-03-23 15:11:04 +0100

    Endrer return type for logEvent og logMessageDetails slik at kafka dependencies ikke blør over i konsumenter av emottak-utils

--- src/main/kotlin/no/nav/emottak/utils/kafka/service/EventLoggingService.kt
+++ src/main/kotlin/no/nav/emottak/utils/kafka/service/EventLoggingService.kt
@@ -4,16 +4,19 @@ import no.nav.emottak.utils.config.EventLogging
 import no.nav.emottak.utils.kafka.client.EventPublisherClient
 import no.nav.emottak.utils.kafka.model.EbmsMessageDetail
 import no.nav.emottak.utils.kafka.model.Event
-import org.apache.kafka.clients.producer.RecordMetadata
 
 class EventLoggingService(
     private val eventLoggingConfig: EventLogging,
     private val kafkaPublisherClient: EventPublisherClient
 ) {
 
-    suspend fun logEvent(event: Event): Result<RecordMetadata> =
+    suspend fun logEvent(event: Event) {
         kafkaPublisherClient.publishMessage(eventLoggingConfig.eventTopic, event.toByteArray())
+            .getOrThrow()
+    }
 
-    suspend fun logMessageDetails(ebmsMessageDetail: EbmsMessageDetail): Result<RecordMetadata> =
+    suspend fun logMessageDetails(ebmsMessageDetail: EbmsMessageDetail) {
         kafkaPublisherClient.publishMessage(eventLoggingConfig.messageDetailsTopic, ebmsMessageDetail.toByteArray())
+            .getOrThrow()
+    }
 }
